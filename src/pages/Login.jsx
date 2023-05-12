5d2b82317431373008c33e9c27fee37951456892
Shahzeb Jadoon <shahzebjadoon0@gmaile.com>
2023-05-12 10:27:28 +0500

    fixed login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
   return (
     <div>
-      <h1>Login</h1>
+      {/* <h1>Login</h1>
       <Button
         text="Login"
         onClick={() => {
@@ -17,7 +17,58 @@ const Login = ({ setIsLoggedIn }) => {
         type="button"
         bgcolor="#1A3A5F"
         color="white"
-      />
+      /> */}
+
+      <div class="contain">
+        <div class="login-form">
+          <h3>Welcome back</h3>
+          <p>Welcome back! Please enter your Details</p>
+          <form action="">
+            <div class="mb-3">
+              <label for="" class="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                name="email"
+                id="email"
+                placeholder="Username@shop.com"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                name="password"
+                id="password"
+                placeholder="*********"
+              />
+            </div>
+            <a href="" class="float-end mb-3">
+              Forgot Password
+            </a>
+            <button
+              type="submit"
+              class="btn btn-primary form-control"
+              onClick={() => {
+                setIsLoggedIn(true);
+                localStorage.setItem("isLoggedIn", true);
+                navigate("/journal");
+              }}
+            >
+              Sign In
+            </button>
+            <p class="orline">or</p>
+            <p class="my-3 text-center">
+              Don’t have an account? <a href="">Sign up for free</a>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };

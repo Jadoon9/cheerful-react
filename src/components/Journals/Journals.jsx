d00ea22d0f9eee4fe133c2187290ea4e227d7db6
rouf0919 <rouf0919@gmail.com>
2023-05-12 09:46:47 +0500

    changes added

--- src/components/Journals/Journals.jsx
+++ src/components/Journals/Journals.jsx
@@ -6,42 +6,39 @@ import Modall from "../Common/Modal";
 const Journals = ({ openModal, handleClose, handleShow }) => {
   return (
     <div className="col-md-3 Journals">
-      <div className="d-flex flex-column">
-        <div className="Journals-top">
-          <h5 className="my-3">Journals</h5>
-          <div class="mb-2">
-            <select class="form-select form-select-lg" name="" id="">
-              <option selected>All</option>
-              <option value="">New Delhi</option>
-              <option value="">Istanbul</option>
-              <option value="">Jakarta</option>
-            </select>
-          </div>
-        </div>
-        <div className="accordion-list">
-          <Accordian
-            header="Daily Journal"
-            cardTitle="Online Consultation"
-            cardTitle2="Online Consultation"
-            id="three"
-          />
-          <Accordian
-            header="Grif Journal"
-            cardTitle="Entry - 01"
-            cardTitle2="Entry - 02"
-            id="four"
-          />
+      <div className="Journals-top py-2">
+        <h5 className="mb-2">Journals</h5>
+        <select class="form-select form-select-lg" name="" id="">
+          <option selected>All</option>
+          <option value="">New Delhi</option>
+          <option value="">Istanbul</option>
+          <option value="">Jakarta</option>
+        </select>
+      </div>
+      <div className="accordion-list">
+        <Accordian
+          header="Daily Journal"
+          cardTitle="Online Consultation"
+          cardTitle2="Online Consultation"
+          id="three"
+        />
+        <Accordian
+          header="Grif Journal"
+          cardTitle="Entry - 01"
+          cardTitle2="Entry - 02"
+          id="four"
+        />
+      </div>
+      <Modall openModal={openModal} handleClose={handleClose} />
 
-          <Modall openModal={openModal} handleClose={handleClose} />
-
-          <Button
-            text="Assign a Journal"
-            onClick={handleShow}
-            type="button"
-            bgcolor="#1A3A5F"
-            color="white"
-          />
-        </div>
+      <div className="mt-auto mx-3">
+        <Button
+          text="Assign a Journal"
+          onClick={handleShow}
+          type="button"
+          bgcolor="#1A3A5F"
+          color="white"
+        />
       </div>
     </div>
   );
